98f1115487b0b3075094cc856b0f556fade71319
Pietro-GAS <117509082+Pietro-GAS@users.noreply.github.com>
2026-04-20 13:17:01 +0000

    Implement set method

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import { HashMap } from "./scripts/hash-map.js";
 
 const map = new HashMap();
 
-console.log(`hash "key1": ${map.hash("key")}`);
-console.log(`hash "key2": ${map.hash("value")}`);
+console.log(`hash "key1": ${map.hash("key1")}`);
+console.log(`hash "key2": ${map.hash("key2")}\n`);
+
+console.log(`hashmap before setting: ${map.elements}`);
+map.set("key1", "value1");
+map.set("key2", "value2");
+console.log(`hashmap after setting: ${map.elements}`);
 
--- src/scripts/hash-map.js
+++ src/scripts/hash-map.js
@@ -2,21 +2,25 @@ export class HashMap {
     constructor() {
         this.loadFactor = 0.75;
         this.capacity = 16;
-        this.elements = [];
+        this.elements = Array(this.capacity);
     }
 
     hash(key) {
         let hashCode = 0;
         const primeNumber = 31;
 
-        for (let i = 0; i < key.length; key++) {
+        for (let i = 0; i < key.length; i++) {
             hashCode = (hashCode * primeNumber + key.charCodeAt(i)) % this.capacity;
         }
         return hashCode
     }
 
     set(key, value) {
-        const haskedKey = this.hash(key);
-        this.elements[hashedKey] = value;
+        const hashedKey = this.hash(key);
+        if (hashedKey < 0 || hashedKey >= this.elements.length) {
+            throw new Error("Trying to access index out of bounds");
+        } else {
+            this.elements[hashedKey] = value;
+        } 
     }
 }
