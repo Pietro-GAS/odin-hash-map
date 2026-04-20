3160ab8e1dc58d602822314530e7d70c022184e8
Pietro-GAS <117509082+Pietro-GAS@users.noreply.github.com>
2026-04-20 13:30:03 +0000

    Implement linked list

---
+++ src/scripts/linked-list.js
@@ -0,0 +1,25 @@
+class Node {
+    constructor(value=null) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+export class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    append(value) {
+        let node = new Node(value);
+        if (!this.head) {
+            this.head = node;
+            return
+        }
+        let current = this.head;
+        while(current.next) {
+            current = current.next;
+        }
+        current.next = node;
+    }
+}
